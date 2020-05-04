2a4f2a2500c83d3953d4817a444f765912da3405
thufir <thufir@dur.bounceme.net>
2020-05-04 16:56:49 -0700

    increment

--- telnet/main.tcl
+++ telnet/main.tcl
@@ -2,13 +2,6 @@ lappend auto_path /home/thufir/NetBeansProjects/spawnTelnet/telnet/api
 
 package require weather 1.0
 
-set stack [tutstack::create]
-foreach num {1 2 3 4 5} { tutstack::push $stack $num }
 
-while { ![tutstack::empty $stack] } {
-    puts "[tutstack::pop $stack]"
-}
-
-tutstack::destroy $stack
 
 tutstack::hello
