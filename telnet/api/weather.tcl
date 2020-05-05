d0039b1f350ea90319c6f9fed74492ba0f46f4ed
thufir <thufir@dur.bounceme.net>
2020-05-04 17:00:49 -0700

    increment

--- telnet/api/weather.tcl
+++ telnet/api/weather.tcl
@@ -2,7 +2,7 @@ package provide weather  1.0
 package require Tcl      8.5
 
 namespace eval ::tutstack {
-    namespace export create destroy push pop peek empty
+#    namespace export create destroy push pop peek empty
 }
 
 
