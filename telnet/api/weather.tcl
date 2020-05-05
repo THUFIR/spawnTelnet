c6c3fe96c9b6e422446fc23bf275cdc6cf75160b
thufir <thufir@dur.bounceme.net>
2020-05-04 17:03:35 -0700

    increment

--- telnet/api/weather.tcl
+++ telnet/api/weather.tcl
@@ -2,11 +2,14 @@ package provide weather  1.0
 package require Tcl      8.5
 
 namespace eval ::tutstack {
-#    namespace export create destroy push pop peek empty
 }
-
-
 
 proc ::tutstack::hello {} {
 	puts "hello world"
+}
+
+
+proc ::tutstack::sum {arg1 arg2} {
+    set x [expr {$arg1 + $arg2}];
+    return $x
 }
