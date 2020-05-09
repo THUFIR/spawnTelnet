43d13a0d83cd8876f4defb5a48f01bf8ff2c85c7
thufir <thufir@dur.bounceme.net>
2020-05-08 21:10:06 -0700

    increment

--- telnet/main.tcl
+++ telnet/main.tcl
@@ -1,5 +1,6 @@
-lappend auto_path /home/thufir/NetBeansProjects/spawnTelnet/telnet/chained
+lappend auto_path /home/thufir/NetBeansProjects/spawnTelnet/telnet/api
 
-package require chained 1.0
+package require weather 1.0
 
-example::first "hmm"
+
+tutstack::connect "nyc"
