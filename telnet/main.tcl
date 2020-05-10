f0596d7ca1865b0289efd557890ee5374278ff85
thufir <thufir@dur.bounceme.net>
2020-05-10 01:30:23 -0700

    increment

--- telnet/main.tcl
+++ telnet/main.tcl
@@ -3,13 +3,22 @@ lappend auto_path /home/thufir/NetBeansProjects/spawnTelnet/telnet/weather
 package require weather 1.0
 
 
+
+
+
+puts "\n\nconnect to wunderground with:"
+puts "-----------------"
+puts "1)\tnoControlFlow"
+puts "2)\tconnect\n\n"
+
+
 set a [gets stdin]
+
 
 if {$a == 1 } {
    wunderground::noControlFlow "nyc"
 } else {
    wunderground::connect "nyc"
-
 }
 
 
