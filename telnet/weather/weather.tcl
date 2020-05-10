13c22fb58f195c84eacb9c8c66b724d9e2d19707
thufir <thufir@dur.bounceme.net>
2020-05-09 23:27:55 -0700

    increment

--- telnet/weather/weather.tcl
+++ telnet/weather/weather.tcl
@@ -4,20 +4,20 @@ package require Expect
 namespace eval ::wunderground {
 }
 
-
-
-proc ::wunderground::parse {city} {
-    global spawn_id
+proc ::wunderground::connect {city} {
+    variable telnet [spawn telnet rainmaker.wunderground.com]
+#    parse $city
     puts "getting weather for $city"
-    expect -nocase "Press Return to continue:"
+    # expect -i $telnet -nocase "Press Return to continue:"
     # You *might* need inter_return instead of return; the documentation isn't clear
-    #interact "\004" return
+    #interact -i $telnet "\004" return
     interact
 }
 
-proc ::wunderground::connect {city} {
-    global spawn_id
-    spawn telnet rainmaker.wunderground.com
-    set telnet $spawn_id
-    parse $city
+proc ::wunderground::parse {city} {
+    variable telnet
+    puts "getting weather for $city"
+    expect -i $telnet -nocase "Press Return to continue:"
+    # You *might* need inter_return instead of return; the documentation isn't clear
+    interact -i $telnet "\004" return
 }
