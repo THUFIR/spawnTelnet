795c43cf6f0aa531126b29689be41dc46f543897
thufir <thufir@dur.bounceme.net>
2020-05-09 23:38:16 -0700

    increment

--- telnet/weather/weather.tcl
+++ telnet/weather/weather.tcl
@@ -4,50 +4,25 @@ package require Expect
 namespace eval ::wunderground {
 }
 
-
-
-
-
+#works
 proc ::wunderground::noControlFlow {city} {
     variable telnet [spawn telnet rainmaker.wunderground.com]
-#    parse $city
     puts "getting weather for $city"
-    # expect -i $telnet -nocase "Press Return to continue:"
-    # You *might* need inter_return instead of return; the documentation isn't clear
-
-#	parse $city
-
-
-#    interact $telnet
-	# "\004" return
     interact
 }
 
 
 
+#hangs
 proc ::wunderground::connect {city} {
     variable telnet [spawn telnet rainmaker.wunderground.com]
-#    parse $city
     puts "getting weather for $city"
-    # expect -i $telnet -nocase "Press Return to continue:"
-    # You *might* need inter_return instead of return; the documentation isn't clear
-
-	parse $city
-
-
-#    interact $telnet
-	# "\004" return
-#    interact
+    parse $city
 }
 
-proc ::wunderground::parse {city} {
-#    variable telnet [spawn telnet rainmaker.wunderground.com]
-#    parse $city
-#    puts "getting weather for $city"
-    # expect -i $telnet -nocase "Press Return to continue:"
-    # You *might* need inter_return instead of return; the documentation isn't clear
 
-    puts "parse $city"
+proc ::wunderground::parse {city} {
+    puts "received $city"
     variable telnet
 
 #    interact $telnet
@@ -55,6 +30,7 @@ proc ::wunderground::parse {city} {
     interact
 }
 
+#works
 proc ::wunderground::old {city} {
     variable telnet
     puts "getting weather for $city"
