90a1aded883b7f711392819036dea81daf2b5599
thufir <thufir@dur.bounceme.net>
2020-05-11 03:12:53 -0700

    increment

--- telnet/weather/locations.tcl
+++ telnet/weather/locations.tcl
@@ -3,24 +3,6 @@ package provide locations 1.0
 namespace eval ::cities {
 }
 
-proc ::cities::loadCities {} {
-
-array set cities {
-	nyc 1
-	lon 2
-}
-
-
-puts "\n\ncities"
-puts "------"
-foreach {c count} [array get cities ] {
-   puts "$c" 
-}
-
-}
-
-
-
 proc ::cities::listCities {} {
 
 
@@ -28,27 +10,5 @@ set cities {{x} {y} {z}}
 
 puts "locations\n--------\n$cities"
 
-return cities
-}
-
-proc ::cities::list {} {
-
-
-puts "list.."
-
-set a(0) "11"
-set a(1) "10"
-set a(2) "20"
-set a(3) "30"
-set a(4) "40"
-
-
-foreach {key value} [array a city] {
-        puts $city
-}
-
-
-
-
-return [array get a]
+return $cities
 }
