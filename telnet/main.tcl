e5828f07b86d133773b1febf97cf3d6e65b26dc7
thufir <thufir@dur.bounceme.net>
2020-05-11 02:10:21 -0700

    increment

--- telnet/main.tcl
+++ telnet/main.tcl
@@ -7,22 +7,25 @@ package require weather 1.0
 
 
 array set cities {
-	nyc
-	lon
+	nyc 1
+	lon 2
 }
 
 
-
+puts "\n\ncities"
+puts "------"
 foreach {c count} [array get cities ] {
-	puts "$c"
+   puts "$c" 
 }
 
 
-puts "\n\nconnect to wunderground with:"
-puts "-----------------"
+puts "\n\n\nconnect to wunderground with:"
+puts "-----------------------------"
 puts "1)\tnoControlFlow"
 puts "2)\tconnect\n\n"
 
+
+puts "connection method:"
 
 set a [gets stdin]
 
